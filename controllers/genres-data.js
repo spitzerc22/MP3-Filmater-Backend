dbb13d12ed353a5b78bd77e749451a3ca765be18
Courtney Spitzer <spitzerc22@gmail.com>
2022-05-03 09:39:42 -0500

    successfully connected to db, but in a novice way

--- controllers/genres-data.js
+++ controllers/genres-data.js
@@ -1,32 +1,4 @@
-const {ObjectId, MongoClient} = require('mongodb')
+const {ObjectId} = require('mongodb')
+const mongoose = require('mongoose')
 
-
-const genreData = class GenreData {
-    //connecting db
-    static async main() {
-        const uri = "mongodb+srv://spitzerc22:HkCUdLhqfYbv5A7y@cluster1.zxjij.mongodb.net/filmater?retryWrites=true&w=majority"
-        const client = new MongoClient(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }, () => {console.log("Connected to DB")})
-        
-        
-        try{
-            //connect to cluster
-            await client.connect();
-            await listDatabases(client)
-        } catch(e) {
-            console.error(e)
-        }
-    }
-
-    static async listDatabases(client) {
-        databasesList = await client.db().admin().listDatabases()
-
-        console.log('Databases')
-        databasesList.databases.forEach(db => console.log( `--${db.name}`))
-    }
-}
-
-genreData.main().catch(console.error)
-module.exprots = genreData
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {console.log("Connected to DB")})
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
+require('dotenv').config()
 
 //db connection
-
+mongoose.connect(
+    process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => {console.log("Connected to DB")}
+)
 
 app.get('/', (req, res) => {
     res.send('This is filmater')
@@ -12,4 +19,4 @@ app.get('*', (req, res) => {
     res.send('Oops, something went wrong.')
 })
 
-app.listen(4242)
+app.listen(process.env.PORT, () => {console.log("Listening on PORT:", process.env.PORT)})
